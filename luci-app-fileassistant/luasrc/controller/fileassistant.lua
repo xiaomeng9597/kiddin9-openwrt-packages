fbd6140ae18c7a013b6badbb8e34e7ac56102ad7
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-07-04 00:01:54 +0800

    Update fileassistant.lua

--- luci-app-fileassistant/luasrc/controller/fileassistant.lua
+++ luci-app-fileassistant/luasrc/controller/fileassistant.lua
@@ -2,10 +2,8 @@ module("luci.controller.fileassistant", package.seeall)
 
 function index()
 
-    entry({"admin", "nas"}, firstchild(), "NAS", 44).dependent = false
-
     local page
-    page = entry({"admin", "system", "fileassistant"}, template("fileassistant"), _("文件助手"), 1)
+    page = entry({"admin", "nas", "fileassistant"}, template("fileassistant"), _("文件助手"), 1)
     page.i18n = "base"
     page.dependent = true
     page.acl_depends = { "luci-app-fileassistant" }
